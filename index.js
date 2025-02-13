e97784585c660d141d091ea81b88a9f0d3e84c3c
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-02-13 14:46:11 +0530

    add_new

--- index.js
+++ index.js
@@ -1,78 +1,81 @@
-
 const products = [
-  { id: 1, name: "P1", price: 25 },
-  { id: 2, name: "P2", price: 45 },
-  { id: 3, name: "P3", price: 30 },
-];
-const cart = {};
-const addToCart = (id) => {
-  const score={};
-  // score["maths"]=95
-  // //score["maths"]+=2
-  // console.log(score);
-  if(cart[id]>=1){
-      increment(id)
-  }else if(!cart[id]){
-  cart[id] = 1;
-  console.log(cart);
-  showCart();
-}
-};
-const increment  =(id) =>{
-  cart[id]+=1;
-  showCart();
-};
-const decrement  =(id) =>{
-  cart[id]-=1;
-  cart[id]<1 && delete cart[id];
-  console.log(cart);
-  showCart();
-};
-const showTotal = () => {
-  let tot=products.reduce((sum,value) => {
-      if(cart[value.id]){
-          return sum +  value.price * cart[value.id]  ;
-  }
-  return sum;
-       
-},0);
-divtot.innerHTML="Total Order amount"+":"+tot
-}
-const showCart = () => {
-  let str = "";
-  products.map((value) => {2
-    if (cart[value.id]) {
+    {
+      id: 1,
+      name: "Product 1",
+      desc: "Description of the product. Description of the product. ",
+      price: 25,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      desc: "Description of the product. Description of the product. ",
+      price: 45,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      desc: "Description of the product. Description of the product. ",
+      price: 30,
+    },
+  ];
+  const cart = {};
+  const addToCart = (id) => {
+    if (!cart[id]) cart[id] = 1;
+    showCart();
+  };
+  const increment = (id) => {
+    cart[id] = cart[id] + 1;
+    showCart();
+  };
+  const decrement = (id) => {
+    cart[id] = cart[id] - 1;
+    cart[id] < 1 && delete cart[id];
+    console.log(cart);
+    showCart();
+  };
+  const showTotal = () => {
+    let total = products.reduce((sum, value) => {
+      return sum + value.price * (cart[value.id] ? cart[value.id] : 0);
+    }, 0);
+  
+    divTotal.innerHTML = `Order Value: $${total}`;
+  };
+  
+  const showCart = () => {
+    let str = "";
+    products.map((value) => {
+      if (cart[value.id]) {
+        str += `
+          <li>${value.name}-$${value.price}-<button onclick='decrement(${
+          value.id
+        })'>-</button>${cart[value.id]}<button onclick='increment(${
+          value.id
+        })'>+</button>-$${value.price * cart[value.id]}</li>
+          `;
+      }
+    });
+    divCart.innerHTML = str;
+    let count = Object.keys(cart).length;
+    items.innerHTML = count;
+    showTotal();
+  };
+  const displayCart = () => {
+    divCartBlock.style.left = "80%"
+  };
+  const hideCart = () => {
+    divCartBlock.style.left = "100%";
+  };
+  const showProducts = () => {
+    let str = "<div class='row'>";
+    products.map((value) => {
       str += `
-      <li>${value.name}-$${value.price}-<button onclick='decrement(${value.id})'>-</button>${cart[value.id]}<button onclick='increment(${value.id})'>+</button>-${value.price*cart[value.id]}</li>
-      `;
-    }
-  });
-  divCart.innerHTML = str;
-  let count=Object.keys(cart).length;
-  items.innerHTML=count;
-  showTotal();
-
-};
-const displaycart = () =>
-{
-//   divcartblock.style.display="block";
-  divcartblock.style.left="80%";
-
-}
-const hidecart =() =>{
-//   divcartblock.style.display="none";
-  divcartblock.style.left="100%";
-
-}
-const showProducts = () => {
-  let str = "";
-  products.map((value) => {
-    str += `
-    <li>${value.id}-${value.name}-${value.price}-<button onclick=addToCart(${value.id})>Add to Cart</button></li>
-    `;
-  });
-  divProducts.innerHTML = str;
-};
-
-//   const tot=score.reduce((sum,value) => sum + value);
-//   console.log(tot);
+        <div class='box'>
+        <h3>${value.name}</h3>
+        <p>${value.desc}</p>
+        <h4>$${value.price}</h4>
+        <button onclick=addToCart(${value.id})>Add to Cart</button>
+        </div>
+        `;
+    });
+    divProducts.innerHTML = str + "</div>";
+  };
